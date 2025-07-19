e69f1592ddc7097b207b57095e374e00ebe7a9ed
SHOOTING_STAR_C <944153243@qq.com>
2025-07-19 16:43:02 +0800

    v1.0.0

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register, StarTools
+from astrbot.api.all import AstrBotConfig
 from astrbot.api import logger
 
-from typing import Union, Optional, Pattern
+from typing import Union, Pattern
 from data.plugins.astrbot_plugin_battlefield_tool.utils.requestUtil import request_api
-from data.plugins.astrbot_plugin_battlefield_tool.database.BattleFiledDataBase import BattleFieldDataBase
-from data.plugins.astrbot_plugin_battlefield_tool.utils.template import bf_main_html_builder, bf_weapons_html_builder, \
-    bf_vehicles_html_builder, bf_servers_html_builder
+from data.plugins.astrbot_plugin_battlefield_tool.database.BattleFiledDataBase import (
+    BattleFieldDataBase,
+)
+from data.plugins.astrbot_plugin_battlefield_tool.utils.template import (
+    bf_main_html_builder,
+    bf_weapons_html_builder,
+    bf_vehicles_html_builder,
+    bf_servers_html_builder,
+)
 
-import re, os, time
+import re
+import time
 
 
 @register("battlefield", "SHOOTING_STAR_C", "一个BATTLEFIELD数据查询插件", "0.1")
 class BattlefieldTool(Star):
-    DEFAULT_GAME = "bfv"  # 默认查询哪个游戏
-    PIC_FOLDER = "pic_path"  # 默认生成图片的位置
-    PIC_PATH = None
-    STAT_PATTERN = re.compile(r'^(\w*)(?:[，,]?game=([\w\-+.]+))?$')  # 正则提取用户名和要查询的游戏
-    LANG_CN = 'zh-cn'
-    LANG_TW = 'zh-tw'
+    STAT_PATTERN = re.compile(
+        r"^(\w*)(?:[，,]?game=([\w\-+.]+))?$"
+    )  # 正则提取用户名和要查询的游戏
+    LANG_CN = "zh-cn"
+    LANG_TW = "zh-tw"
 
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
+        self.default_game = config.get("default_game")
+        self.timeout_config = config.get("timeout_config")
+        self.img_quality = config.get("img_quality")
         self.bf_data_path = StarTools.get_data_dir("battleFiled_tool_plugin")
-        self.PIC_PATH = os.path.join(self.bf_data_path, self.PIC_FOLDER)
         self.db = BattleFieldDataBase(self.bf_data_path)  # 初始化数据库
 
     async def initialize(self):
@@ -36,26 +46,33 @@ class BattlefieldTool(Star):
         lang = self.LANG_CN
         qq_id = event.get_sender_id()
         # 解析命令
-        ea_name, game = self._parse_input_regex(['stat'], self.STAT_PATTERN, message_str)
+        ea_name, game = self._parse_input_regex(
+            ["stat"], self.STAT_PATTERN, message_str, self.default_game
+        )
         # 如果没有传入ea_name则查询已绑定的
         if ea_name is None:
             bing_data = self._query_bind_user(qq_id)
             if bing_data is None:
                 raise ValueError("请先使用bind [ea_name]绑定")
             else:
-                ea_name = bing_data['ea_name']
+                ea_name = bing_data["ea_name"]
         logger.info(f"玩家id:{ea_name}，所查询游戏:{game}")
         # 战地1使用繁中
         if game == "bf1":
             lang = self.LANG_TW
         # 调用API查询玩家数据
-        player_data = await request_api(game, "all", {'name': ea_name, 'lang': lang, 'platform': 'pc'})
+        player_data = await request_api(
+            game,
+            "all",
+            {"name": ea_name, "lang": lang, "platform": "pc"},
+            self.timeout_config,
+        )
         if player_data is None:
             yield event.plain_result("API调用失败，没有响应任何信息")
         if player_data.get("code") != 200:
             yield event.plain_result(player_data.get("errors")[0])
-        if player_data.get('code') == 200:
-            player_data['__update_time'] = time.time()
+        if player_data.get("code") == 200:
+            player_data["__update_time"] = time.time()
             pic_url = await self._main_data_to_pic(player_data, game)
             yield event.image_result(pic_url)
 
@@ -66,26 +83,33 @@ class BattlefieldTool(Star):
         lang = self.LANG_CN
         qq_id = event.get_sender_id()
         # 解析命令
-        ea_name, game = self._parse_input_regex(['weapons', '武器'], self.STAT_PATTERN, message_str)
+        ea_name, game = self._parse_input_regex(
+            ["weapons", "武器"], self.STAT_PATTERN, message_str, self.default_game
+        )
         # 如果没有传入ea_name则查询已绑定的
         if ea_name is None:
             bing_data = self._query_bind_user(qq_id)
             if bing_data is None:
                 raise ValueError("请先使用bind [ea_name]绑定")
             else:
-                ea_name = bing_data['ea_name']
+                ea_name = bing_data["ea_name"]
         logger.info(f"玩家id:{ea_name}，所查询游戏:{game}")
         # 战地1使用繁中
         if game == "bf1":
             lang = self.LANG_TW
         # 调用API查询玩家数据
-        player_data = await request_api(game, "weapons", {'name': ea_name, 'lang': lang, 'platform': 'pc'})
+        player_data = await request_api(
+            game,
+            "weapons",
+            {"name": ea_name, "lang": lang, "platform": "pc"},
+            self.timeout_config,
+        )
         if player_data is None:
             yield event.plain_result("API调用失败，没有响应任何信息")
         if player_data.get("code") != 200:
             yield event.plain_result(player_data.get("errors")[0])
-        if player_data.get('code') == 200:
-            player_data['__update_time'] = time.time()
+        if player_data.get("code") == 200:
+            player_data["__update_time"] = time.time()
             pic_url = await self._weapons_data_to_pic(player_data, game)
             yield event.image_result(pic_url)
 
@@ -96,26 +120,33 @@ class BattlefieldTool(Star):
         lang = self.LANG_CN
         qq_id = event.get_sender_id()
         # 解析命令
-        ea_name, game = self._parse_input_regex(['vehicles', '载具'], self.STAT_PATTERN, message_str)
+        ea_name, game = self._parse_input_regex(
+            ["vehicles", "载具"], self.STAT_PATTERN, message_str, self.default_game
+        )
         # 如果没有传入ea_name则查询已绑定的
         if ea_name is None:
             bing_data = self._query_bind_user(qq_id)
             if bing_data is None:
                 raise ValueError("请先使用bind [ea_name]绑定")
             else:
-                ea_name = bing_data['ea_name']
+                ea_name = bing_data["ea_name"]
         logger.info(f"玩家id:{ea_name}，所查询游戏:{game}")
         # 战地1使用繁中
         if game == "bf1":
             lang = self.LANG_TW
         # 调用API查询玩家数据
-        player_data = await request_api(game, "vehicles", {'name': ea_name, 'lang': lang, 'platform': 'pc'})
+        player_data = await request_api(
+            game,
+            "vehicles",
+            {"name": ea_name, "lang": lang, "platform": "pc"},
+            self.timeout_config,
+        )
         if player_data is None:
             yield event.plain_result("API调用失败，没有响应任何信息")
         if player_data.get("code") != 200:
             yield event.plain_result(player_data.get("errors")[0])
-        if player_data.get('code') == 200:
-            player_data['__update_time'] = time.time()
+        if player_data.get("code") == 200:
+            player_data["__update_time"] = time.time()
             pic_url = await self._vehicles_data_to_pic(player_data, game)
             yield event.image_result(pic_url)
 
@@ -125,7 +156,9 @@ class BattlefieldTool(Star):
         message_str = event.message_str
         lang = self.LANG_CN
         # 解析命令
-        server_name, game = self._parse_input_regex(['servers', '服务器'], self.STAT_PATTERN, message_str)
+        server_name, game = self._parse_input_regex(
+            ["servers", "服务器"], self.STAT_PATTERN, message_str, self.default_game
+        )
         if server_name is None:
             raise ValueError("不能查所有哦~")
         logger.info(f"查询服务器:{server_name}，所查询游戏:{game}")
@@ -133,17 +166,29 @@ class BattlefieldTool(Star):
         if game == "bf1":
             lang = self.LANG_TW
         # 调用API查询玩家数据
-        servers_data = await request_api(game, "servers",
-                                         {'name': server_name, 'lang': lang, 'platform': 'pc', 'region': 'all',
-                                          'limit': 10})
+        servers_data = await request_api(
+            game,
+            "servers",
+            {
+                "name": server_name,
+                "lang": lang,
+                "platform": "pc",
+                "region": "all",
+                "limit": 30,
+            },
+            self.timeout_config,
+        )
         if servers_data is None:
             yield event.plain_result("API调用失败，没有响应任何信息")
         if servers_data.get("code") != 200:
             yield event.plain_result(servers_data.get("errors")[0])
-        if servers_data.get('code') == 200:
-            servers_data['__update_time'] = time.time()
-            pic_url = await self._servers_data_to_pic(servers_data, game)
-            yield event.image_result(pic_url)
+        if servers_data.get("code") == 200:
+            if servers_data["servers"] is not None and len(servers_data["servers"]) > 0:
+                servers_data["__update_time"] = time.time()
+                pic_url = await self._servers_data_to_pic(servers_data, game)
+                yield event.image_result(pic_url)
+            else:
+                yield event.plain_result("暂无数据")
 
     @filter.command("bind", alias=["绑定"])
     async def bf_bind(self, event: AstrMessageEvent):
@@ -151,22 +196,34 @@ class BattlefieldTool(Star):
         message_str = event.message_str
         qq_id = event.get_sender_id()
         # 先查询是否绑定过
-        ea_name, game = self._parse_input_regex(['bind', '绑定'], None, message_str)
+        ea_name, game = self._parse_input_regex(
+            ["bind", "绑定"], None, message_str, self.default_game
+        )
         # 调用bfv的接口查询用户是否存在
-        player_data = await request_api('bfv', "stats", {'name': ea_name, 'lang': 'zh-cn', 'platform': 'pc'})
+        player_data = await request_api(
+            self.default_game,
+            "stats",
+            {"name": ea_name, "lang": "zh-cn", "platform": "pc"},
+            self.timeout_config,
+        )
         if player_data is None:
             yield event.plain_result("API调用失败，没有响应任何信息")
         if player_data.get("code") != 200:
             yield event.plain_result(player_data.get("errors")[0])
-        if player_data.get('code') == 200:
-            ea_id = player_data['userId']
+        if player_data.get("code") == 200:
+            ea_id = player_data["userId"]
             logger.debug(f"已查询到{ea_name}的ea_id：{ea_id}")
             # 持久化绑定数据
             msg = self.upsert_user_bind(qq_id, ea_name, ea_id)
             yield event.plain_result(msg)
 
-    def _parse_input_regex(self, str_to_remove_list: list[str], pattern: Union[Pattern[str], None], base_string: str,
-                           default_game: str = "bfv"):
+    def _parse_input_regex(
+        self,
+        str_to_remove_list: list[str],
+        pattern: Union[Pattern[str], None],
+        base_string: str,
+        default_game: str,
+    ):
         """私有方法：从base_string中移除str_to_remove_list并去空格，然后根据正则取出参数
         Args:
             str_to_remove_list: 需要移除的子串list
@@ -177,8 +234,8 @@ class BattlefieldTool(Star):
         """
         # 移除目标子串和空格
         for str_to_remove in str_to_remove_list:
-            base_string = base_string.replace(str_to_remove, '')
-        clean_str = base_string.replace(' ', '')
+            base_string = base_string.replace(str_to_remove, "")
+        clean_str = base_string.replace(" ", "")
         # 用正则提取输入的参数
         if pattern is not None:
             match = pattern.match(clean_str.strip())
@@ -199,18 +256,21 @@ class BattlefieldTool(Star):
             ea_name: 游戏ID名称
             ea_id: EA账号ID
         Returns:
-            返回更新前的旧数据（如果没有旧数据则返回None）
+            返回绑定消息
         """
         # 1. 尝试获取旧数据
         old_data = self._query_bind_user(qq_id)
         # 2. 执行插入或更新
-        self.db.exec_sql("""
+        self.db.exec_sql(
+            """
                          INSERT INTO battleField_user_binds (qq_id, ea_name, ea_id)
                          VALUES (?, ?, ?) ON CONFLICT(qq_id) DO
                          UPDATE SET
                              ea_name = excluded.ea_name,
                              ea_id = excluded.ea_id
-                         """, (qq_id, ea_name, ea_id))
+                         """,
+            (qq_id, ea_name, ea_id),
+        )
         if old_data is not None:
             msg = f"更新绑定记数据: {old_data['ea_name']}-->{ea_name}"
         else:
@@ -226,7 +286,9 @@ class BattlefieldTool(Star):
             返回数据（没有则返回None）
         """
         return self.db.query(
-            "SELECT * FROM battleField_user_binds WHERE qq_id = ?", (qq_id,), fetch_all=False
+            "SELECT * FROM battleField_user_binds WHERE qq_id = ?",
+            (qq_id,),
+            fetch_all=False,
         )
 
     async def terminate(self):
@@ -240,9 +302,16 @@ class BattlefieldTool(Star):
             返回生成的图片
         """
         html = bf_main_html_builder(data, game)
-        url = await self.html_render(html, {}, True,
-                                     {"timeout": 10000, "quality": 100,
-                                      "clip": {"x": 0, "y": 0, "width": 700, "height": 2353}})
+        url = await self.html_render(
+            html,
+            {},
+            True,
+            {
+                "timeout": 10000,
+                "quality": self.img_quality,
+                "clip": {"x": 0, "y": 0, "width": 700, "height": 2353},
+            },
+        )
         return url
 
     async def _weapons_data_to_pic(self, data: dict, game: str):
@@ -253,9 +322,16 @@ class BattlefieldTool(Star):
             返回生成的图片
         """
         html = bf_weapons_html_builder(data, game)
-        url = await self.html_render(html, {}, True,
-                                     {"timeout": 10000, "quality": 100,
-                                      "clip": {"x": 0, "y": 0, "width": 700, "height": 10000}})
+        url = await self.html_render(
+            html,
+            {},
+            True,
+            {
+                "timeout": 10000,
+                "quality": self.img_quality,
+                "clip": {"x": 0, "y": 0, "width": 700, "height": 10000},
+            },
+        )
         return url
 
     async def _vehicles_data_to_pic(self, data: dict, game: str):
@@ -266,9 +342,16 @@ class BattlefieldTool(Star):
             返回生成的图片
         """
         html = bf_vehicles_html_builder(data, game)
-        url = await self.html_render(html, {}, True,
-                                     {"timeout": 10000, "quality": 100,
-                                      "clip": {"x": 0, "y": 0, "width": 700, "height": 10000}})
+        url = await self.html_render(
+            html,
+            {},
+            True,
+            {
+                "timeout": 10000,
+                "quality": self.img_quality,
+                "clip": {"x": 0, "y": 0, "width": 700, "height": 10000},
+            },
+        )
         return url
 
     async def _servers_data_to_pic(self, data: dict, game: str):
@@ -280,12 +363,19 @@ class BattlefieldTool(Star):
         """
         # 数据量较少时设置高度
         height = 10000
-        if data['servers'] is not None and len(data['servers']) == 1:
+        if data["servers"] is not None and len(data["servers"]) == 1:
             height = 450
-        elif data['servers'] is not None and len(data['servers']) == 2:
-            height = 650
+        elif data["servers"] is not None and len(data["servers"]) == 2:
+            height = 620
         html = bf_servers_html_builder(data, game)
-        url = await self.html_render(html, {}, True,
-                                     {"timeout": 10000, "quality": 100,
-                                      "clip": {"x": 0, "y": 0, "width": 700, "height": height}})
+        url = await self.html_render(
+            html,
+            {},
+            True,
+            {
+                "timeout": 10000,
+                "quality": self.img_quality,
+                "clip": {"x": 0, "y": 0, "width": 700, "height": height},
+            },
+        )
         return url
